f08f3a9f51b0711f417be3a03d0c8fce98ebe690
swiersyoram <yoram@swiers.be>
2022-04-29 11:32:10 +0200

    added header logo

--- antwerp_velo_stations/src/Components/Layout/Header.tsx
+++ antwerp_velo_stations/src/Components/Layout/Header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-
+// import logo from "./public/assets/FullLogo.svg";
 type Props = {};
 
 function Header({}: Props) {
   return (
-    <header className="fixed top-0 left-0 z-50 bg-red-500">my header</header>
+    <header className="fixed top-0 left-0 z-50 bg-red-500">
+      <a href="/">
+        <img src="/assets/images/FullLogo.svg" alt="" className="w-32 h-full" />
+      </a>
+    </header>
   );
 }
 
