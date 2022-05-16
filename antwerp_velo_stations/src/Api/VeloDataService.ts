39869296f187c51d115d719c0b1c6528caad3e38
swiersyoram <yoram@swiers.be>
2022-05-16 10:24:06 +0200

    udpate api path

--- antwerp_velo_stations/src/Api/VeloDataService.ts
+++ antwerp_velo_stations/src/Api/VeloDataService.ts
@@ -1,10 +1,16 @@
 import axios, { AxiosPromise } from "axios";
 import React from "react";
 
-const apiRoute: string = "https://api.citybik.es/v2/networks/velo-antwerpen";
+const apiRoute: string = "http://localhost:8000/stations";
+const AuthStr: string =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NTI2ODkyODUsImV4cCI6MTY4NDIyNTI4NX0.4VvnqIb5GGO686iWX4PY56v4v-6I9Fr4Spu-hw2hIaw";
 export function GetAllStations() {
-  return axios.get(apiRoute).then(({ data }) => {
-    // console.log(res);
-    return data.network.stations;
-  });
+  // console.log("with new string");
+
+  return axios
+    .get(apiRoute, { headers: { Authorization: AuthStr } })
+    .then(({ data }) => {
+      // console.log(res);
+      return data.network.stations;
+    });
 }
